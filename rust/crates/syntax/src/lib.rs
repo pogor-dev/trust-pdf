a21907926181437f92253e9af9c19988c4967f76
Victor Pogor <victor.pogor@outlook.com>
2025-08-09 15:10:54 +1000

    moved utility_types

--- rust/crates/syntax/src/lib.rs
+++ rust/crates/syntax/src/lib.rs
@@ -2,10 +2,13 @@
 mod arc;
 // mod cursor;
 mod green;
-mod utilities;
+mod utility_types;
+
+#[cfg(test)]
+mod utility_types_tests;
 
 pub use crate::{
     // api::{language::Language, trivia::SyntaxTrivia},
     green::SyntaxKind,
-    utilities::node_or_token::NodeOrToken,
+    utility_types::NodeOrToken,
 };
--- rust/crates/syntax/src/utilities.rs
+++
@@ -1,4 +0,0 @@
-pub(crate) mod node_or_token;
-
-#[cfg(test)]
-mod node_or_token_tests;
