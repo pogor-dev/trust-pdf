5b566a61143c0c1f96e217b2907c2641c6d169f7
Victor Pogor <victor.pogor@outlook.com>
2025-10-08 23:49:18 +1000

    changes on trivia mod

--- rust/crates/syntax/src/green/trivia.rs
+++ rust/crates/syntax/src/green/trivia.rs
@@ -1,8 +1,8 @@
-use std::ptr::NonNull;
+use std::{ptr::NonNull, sync::Arc};
 
 use countme::Count;
 
-use crate::SyntaxKind;
+use crate::{SyntaxKind, green::arena::GreenTree};
 
 #[derive(Debug, Default)]
 pub struct GreenTriviaList<'trivia> {
@@ -10,7 +10,8 @@ pub struct GreenTriviaList<'trivia> {
     full_width: usize,
 }
 
-#[repr(packed)] // Makes this 4 bytes instead of 8 bytes.
+#[repr(packed)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub(super) struct GreenTriviaHead {
     kind: SyntaxKind,       // 2 bytes
     full_width: u16,        // 2 bytes
@@ -19,9 +20,17 @@ pub(super) struct GreenTriviaHead {
 
 /// This is used to store the trivia in the arena.
 /// The actual text is stored inline after the head.
+#[derive(Debug, Eq, PartialEq, Hash)]
+#[repr(C)]
 pub(super) struct GreenTriviaData {
     head: GreenTriviaHead,
     text: [u8; 0],
+}
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub struct GreenTrivia {
+    token: GreenTriviaInTree,
+    arena: Arc<GreenTree>,
 }
 
 #[derive(Clone, Copy)]
@@ -38,7 +47,3 @@ impl GreenTriviaInTree {
 // SAFETY: The pointer is valid.
 unsafe impl Send for GreenTriviaInTree {}
 unsafe impl Sync for GreenTriviaInTree {}
-
-// TODO: no need for memory allocation for EOL, space
-// TODO: this can't be done here if we want to keep this crate abstracted like rowan,
-// TODO: we don't know the SyntaxKind here
