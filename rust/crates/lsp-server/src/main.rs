953907498c423693ffebdafcbd27e19f5776d88f
Victor Pogor <victor.pogor@outlook.com>
2026-01-07 21:43:30 +1000

    feat(lsp-server): implement initial LSP server structure and dependencies

---
+++ rust/crates/lsp-server/src/main.rs
@@ -0,0 +1,73 @@
+use std::error::Error;
+use std::fs;
+
+use lsp_server::{Connection, ExtractError, Message, RequestId, Response};
+use lsp_types::{GotoDefinitionResponse, InitializeParams, Location, OneOf, ServerCapabilities, request::GotoDefinition};
+
+fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
+    // Note that  we must have our logging only write out to stderr since the communication with the client
+    // is done via stdin/stdout. If we write to stdout, we will corrupt the communication.
+    eprintln!("Starting WASM based LSP server");
+
+    // Create the transport. Includes the stdio (stdin and stdout) versions but this could
+    // also be implemented to use sockets or HTTP.
+    let (connection, io_threads) = Connection::stdio();
+
+    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
+    let server_capabilities = serde_json::to_value(&ServerCapabilities {
+        definition_provider: Some(OneOf::Left(true)),
+        ..Default::default()
+    })
+    .unwrap();
+    let initialization_params = connection.initialize(server_capabilities)?;
+    main_loop(connection, initialization_params)?;
+    io_threads.join()?;
+
+    // Shut down gracefully.
+    eprintln!("Shutting down server");
+    Ok(())
+}
+
+fn main_loop(connection: Connection, params: serde_json::Value) -> Result<(), Box<dyn Error + Sync + Send>> {
+    let _params: InitializeParams = serde_json::from_value(params).unwrap();
+    for msg in &connection.receiver {
+        match msg {
+            Message::Request(req) => {
+                if connection.handle_shutdown(&req)? {
+                    return Ok(());
+                }
+                match cast::<GotoDefinition>(req.clone()) {
+                    Ok((id, params)) => {
+                        let uri = params.text_document_position_params.text_document.uri;
+                        eprintln!("Received gotoDefinition request #{} {}", id, uri.to_string());
+                        let loc = Location::new(uri, lsp_types::Range::new(lsp_types::Position::new(0, 0), lsp_types::Position::new(0, 0)));
+                        let mut vec = Vec::new();
+                        vec.push(loc);
+                        let result = Some(GotoDefinitionResponse::Array(vec));
+                        let result = serde_json::to_value(&result).unwrap();
+                        let resp = Response {
+                            id,
+                            result: Some(result),
+                            error: None,
+                        };
+                        connection.sender.send(Message::Response(resp))?;
+                        continue;
+                    }
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
+            }
+            Message::Response(_resp) => {}
+            Message::Notification(_not) => {}
+        }
+    }
+    Ok(())
+}
+
+fn cast<R>(req: lsp_server::Request) -> Result<(RequestId, R::Params), ExtractError<lsp_server::Request>>
+where
+    R: lsp_types::request::Request,
+    R::Params: serde::de::DeserializeOwned,
+{
+    req.extract(R::METHOD)
+}
