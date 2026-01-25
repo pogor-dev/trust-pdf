cefa040e70dcbb938c89d1a3756e9273cddd3d99
Victor Pogor <victor.pogor@outlook.com>
2026-01-25 17:16:07 +1000

    feat(parser, syntax): implement PdfDocumentSyntax and PdfDocumentInnerSyntax structures

--- rust/crates/syntax/src/green_nodes/nodes.rs
+++ rust/crates/syntax/src/green_nodes/nodes.rs
@@ -1,1 +1,16 @@
-pub struct PdfDocumentSyntax {}
+use crate::{GreenNode, SyntaxKind};
+
+// TODO: lex the PDF version separately? Might be false positive inside the document
+#[derive(Clone)]
+pub struct PdfDocumentSyntax {
+    kind: SyntaxKind,
+    bodies: PdfDocumentInnerSyntax,
+}
+
+#[derive(Clone)]
+pub struct PdfDocumentInnerSyntax {
+    kind: SyntaxKind,
+    objects: GreenNode,
+    xref_table: GreenNode,
+    trailer: GreenNode,
+}
